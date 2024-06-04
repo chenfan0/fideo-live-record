a08df15b3414f6768593663ad8f913ff594625ad
chen fan <1490040662@qq.com>
2024-06-04 12:43:45 +0800

    init

---
+++ electron.vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'path'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve(__dirname, 'src/renderer/src'),
+        '@': resolve(__dirname, 'src/renderer/src')
+      }
+    },
+    plugins: [react()]
+  }
+})
---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+function App(): JSX.Element {
+  return (
+    <>
+      <div className=""></div>
+    </>
+  )
+}
+
+export default App
