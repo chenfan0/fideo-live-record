9d29de513f29e0d536e90624d8251b7d70489234
chen fan <1490040662@qq.com>
2024-06-11 20:13:27 +0800

    feat(crawler): support huajiao

--- src/main/crawler/plugins/huajiao.js
+++ src/main/crawler/plugins/huajiao.js
@@ -1,73 +1,68 @@
-import debug from "debug"
+import debug from 'debug'
 
-import { request } from "../request"
-import { handleErrMsg } from "../handleErrMsg"
-import { SUCCESS_CODE } from "../../../error"
-import { NOT_URLS } from "../const"
+import { request } from '../base-request.js'
+import { captureError } from '../capture-error.js'
 
-const log = debug('fideo-live-stream-getHuaJiaoLiveUrl')
+import { SUCCESS_CODE } from '../../../code'
 
-export async function getHuaJiaoLiveUrl(roomId, others = {}) {
+const log = debug('fideo-crawler-huajiao')
+
+async function baseGetHuaJiaoLiveUrlsPlugin(roomId, others = {}) {
   const { proxy, cookie } = others
-  log('getHuaJiaoLiveUrl start: ', roomId, cookie, proxy)
 
-  try {
-    const htmlContent = (await request(`https://www.huajiao.com/l/${roomId}`, {
+  log('roomId:', roomId, 'cookie:', cookie, 'proxy:', proxy)
+
+  const htmlContent = (
+    await request(`https://www.huajiao.com/l/${roomId}`, {
       headers: {
         cookie
       },
       proxy
-    })).data
+    })
+  ).data
 
-    const scriptContentRegex = /<script\b[^>]*>([\s\S]*?)<\/script>/gi
-    const matches = htmlContent.match(scriptContentRegex)
+  const scriptContentRegex = /<script\b[^>]*>([\s\S]*?)<\/script>/gi
+  const matches = htmlContent.match(scriptContentRegex)
 
-    let str = ''
+  let str = ''
 
-    for (const match of matches) {
-      if (
-        match.includes('author') &&
-        match.includes('feed')
-      ) {
-        str = match.replace(scriptContentRegex, '$1')
-        break
+  for (const match of matches) {
+    if (match.includes('author') && match.includes('feed')) {
+      str = match.replace(scriptContentRegex, '$1')
+      break
+    }
+  }
+
+  const fn = new Function(`
+    const window = {
+      HJ_JS_CONF: {},
+      _DATA: {},
+    };
+    ${str};
+    return window
+  `)
+  const data = fn()
+  const uid = data._DATA.author.uid
+  const sn = data._DATA.feed.feed.sn
+
+  const liveUrlData = (
+    await request(
+      `https://live.huajiao.com/live/substream?&sn=${sn}&uid=${uid}&liveid=${roomId}&encode=h264`,
+      {
+        headers: {
+          cookie
+        },
+        proxy
       }
-    }
+    )
+  ).data
 
-    const fn = new Function(`
-      const window = {
-        HJ_JS_CONF: {},
-        _DATA: {},
-      };
-      ${str};
-      return window
-    `)
-    const data = fn()
-    const uid = data._DATA.author.uid
-    const sn = data._DATA.feed.feed.sn
+  const liveUrls = [liveUrlData.data.main, liveUrlData.data.h264_url]
 
-    const liveUrlData = (await request(`https://live.huajiao.com/live/substream?&sn=${sn}&uid=${uid}&liveid=${roomId}&encode=h264`, {
-      headers: {
-        cookie
-      },
-      proxy
-    })).data
-
-    const liveUrlObj = {
-      best: [liveUrlData.data.main, liveUrlData.data.h264_url],
-      origin: [liveUrlData.url, liveUrlData.data.h264_url]
-    }
-
-    return {
-      liveUrlObj,
-      code: SUCCESS_CODE
-    }
-
-  } catch (e) {
-    const errMsg = e.message
-
-    log('getHuaJiaoLiveUrl error: ', errMsg)
-
-    return handleErrMsg(errMsg)
+  return {
+    code: SUCCESS_CODE,
+    liveUrls
   }
 }
+
+export const getHuaJiaoLiveUrlsPlugin = captureError(baseGetHuaJiaoLiveUrlsPlugin)
