fdd8bbcb96d59fc44fbd6c3e22a0af99a586485a
chen fan <1490040662@qq.com>
2024-06-11 20:14:03 +0800

    feat(crawler): support twitch

--- src/main/crawler/plugins/twitch.js
+++ src/main/crawler/plugins/twitch.js
@@ -1,109 +1,104 @@
 import debug from 'debug'
 
-import { request } from '../request.js'
-import { ERROR_CODE, SUCCESS_CODE, ERROR_MESSAGE } from '../../../error'
-import { handleErrMsg } from '../handleErrMsg.js'
+import { request } from '../base-request.js'
+import { captureError } from '../capture-error.js'
 
-const log = debug('fideo-live-stream-getTwitchLiveUrl')
+import { CRAWLER_ERROR_CODE, SUCCESS_CODE } from '../../../code'
 
-export async function getTwitchLiveUrl(roomId, others = {}) {
+const log = debug('fideo-crawler-twitch')
+
+async function baseGetTwitchLiveUrlsPlugin(roomId, others = {}) {
   const { proxy, cookie } = others
 
-  log('getTwitchUrl start: ', roomId, cookie, proxy)
+  log('roomId:', roomId, 'cookie:', cookie, 'proxy:', proxy)
+
+  const html = (
+    await request(`https://www.twitch.tv/${roomId}`, {
+      headers: {
+        cookie
+      },
+      proxy
+    })
+  ).data
+
+  const clientIDIndex = html.indexOf('clientId')
+  const firstQuotationIndex = html.indexOf('"', clientIDIndex)
+  const secondQuotationIndex = html.indexOf('"', firstQuotationIndex + 1)
+  const clientID = html.slice(firstQuotationIndex + 1, secondQuotationIndex)
+  const query = {
+    operationName: 'PlaybackAccessToken_Template',
+    query:
+      'query PlaybackAccessToken_Template($login: String!, $isLive: Boolean!, $vodID: ID!, $isVod: Boolean!, $playerType: String!) {  streamPlaybackAccessToken(channelName: $login, params: {platform: "web", playerBackend: "mediaplayer", playerType: $playerType}) @include(if: $isLive) {    value    signature   authorization { isForbidden forbiddenReasonCode }   __typename  }  videoPlaybackAccessToken(id: $vodID, params: {platform: "web", playerBackend: "mediaplayer", playerType: $playerType}) @include(if: $isVod) {    value    signature   __typename  }}',
+    variables: {
+      isLive: true,
+      login: roomId,
+      isVod: false,
+      vodID: '',
+      playerType: 'site'
+    }
+  }
 
   try {
-    const html = (
-      await request(`https://www.twitch.tv/${roomId}`, {
-        headers: {
-          cookie
-        },
-        proxy
-      })
-    ).data
-
-    const clientIDIndex = html.indexOf('clientId')
-    const firstQuotationIndex = html.indexOf('"', clientIDIndex)
-    const secondQuotationIndex = html.indexOf('"', firstQuotationIndex + 1)
-    const clientID = html.slice(firstQuotationIndex + 1, secondQuotationIndex)
-    const query = {
-      operationName: 'PlaybackAccessToken_Template',
-      query:
-        'query PlaybackAccessToken_Template($login: String!, $isLive: Boolean!, $vodID: ID!, $isVod: Boolean!, $playerType: String!) {  streamPlaybackAccessToken(channelName: $login, params: {platform: "web", playerBackend: "mediaplayer", playerType: $playerType}) @include(if: $isLive) {    value    signature   authorization { isForbidden forbiddenReasonCode }   __typename  }  videoPlaybackAccessToken(id: $vodID, params: {platform: "web", playerBackend: "mediaplayer", playerType: $playerType}) @include(if: $isVod) {    value    signature   __typename  }}',
-      variables: {
-        isLive: true,
-        login: roomId,
-        isVod: false,
-        vodID: '',
-        playerType: 'site'
-      }
-    }
-
-    try {
-      const { data: res } = await request('https://gql.twitch.tv/gql', {
-        method: 'post',
-        headers: {
-          'Client-Id': clientID,
-          cookie
-        },
-        proxy,
-        data: {
-          operationName: 'UseLive',
-          extensions: {
-            persistedQuery: {
-              version: 1,
-              sha256Hash: '639d5f11bfb8bf3053b424d9ef650d04c4ebb7d94711d644afb08fe9a0fad5d9'
-            }
-          },
-          variables: {
-            channelLogin: roomId
+    const { data: res } = await request('https://gql.twitch.tv/gql', {
+      method: 'post',
+      headers: {
+        'Client-Id': clientID,
+        cookie
+      },
+      proxy,
+      data: {
+        operationName: 'UseLive',
+        extensions: {
+          persistedQuery: {
+            version: 1,
+            sha256Hash: '639d5f11bfb8bf3053b424d9ef650d04c4ebb7d94711d644afb08fe9a0fad5d9'
           }
-        }
-      })
-      const data = res.data
-      const stream = data.user.stream
-      if (!stream) {
-        return {
-          code: ERROR_CODE.NOT_URLS
+        },
+        variables: {
+          channelLogin: roomId
         }
       }
-    } catch (e) {}
-
-    const json = (
-      await request('https://gql.twitch.tv/gql', {
-        method: 'post',
-        headers: {
-          'Client-Id': clientID,
-          cookie
-        },
-        proxy,
-        data: query
-      })
-    ).data
-
-    const streamPlaybackAccessToken = json.data.streamPlaybackAccessToken
-    const search = new URLSearchParams('')
-    search.set('acmb', 'e30=')
-    search.set('allow_source', true)
-    search.set('fast_bread', true)
-    search.set('player_backend', true)
-    search.set('reassignments_supported', true)
-    search.set('sig', streamPlaybackAccessToken.signature)
-    search.set('supported_codecs', 'avc1')
-    search.set('transcode_mode', 'cbr_v1')
-    search.set('token', streamPlaybackAccessToken.value)
-    const url = `https://usher.ttvnw.net/api/channel/hls/${roomId}.m3u8?${search.toString()}`
-
-    return {
-      code: SUCCESS_CODE,
-      liveUrlObj: {
-        best: [url],
-        original: [url]
+    })
+    const data = res.data
+    const stream = data.user.stream
+    if (!stream) {
+      return {
+        code: CRAWLER_ERROR_CODE.NOT_URLS
       }
     }
   } catch (e) {
-    const errMsg = e.message
-    log('getTwitchLiveUrl error: ', errMsg)
+    log('baseGetTwitchLiveUrlPlugin error: ', e.message)
+  }
 
-    return handleErrMsg(errMsg)
+  const json = (
+    await request('https://gql.twitch.tv/gql', {
+      method: 'post',
+      headers: {
+        'Client-Id': clientID,
+        cookie
+      },
+      proxy,
+      data: query
+    })
+  ).data
+
+  const streamPlaybackAccessToken = json.data.streamPlaybackAccessToken
+  const search = new URLSearchParams('')
+  search.set('acmb', 'e30=')
+  search.set('allow_source', true)
+  search.set('fast_bread', true)
+  search.set('player_backend', true)
+  search.set('reassignments_supported', true)
+  search.set('sig', streamPlaybackAccessToken.signature)
+  search.set('supported_codecs', 'avc1')
+  search.set('transcode_mode', 'cbr_v1')
+  search.set('token', streamPlaybackAccessToken.value)
+  const url = `https://usher.ttvnw.net/api/channel/hls/${roomId}.m3u8?${search.toString()}`
+
+  return {
+    code: SUCCESS_CODE,
+    liveUrls: [url]
   }
 }
+
+export const getTwitchLiveUrlsPlugin = captureError(baseGetTwitchLiveUrlsPlugin)
