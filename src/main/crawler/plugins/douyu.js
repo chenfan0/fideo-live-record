486ac964988e0285ff2c58049df2c5218f4a3301
chen fan <1490040662@qq.com>
2024-06-11 20:13:18 +0800

    feat(crawler): support douyu

--- src/main/crawler/plugins/douyu.js
+++ src/main/crawler/plugins/douyu.js
@@ -1,18 +1,45 @@
 import CryptoJS from 'crypto-js'
 import debug from 'debug'
 
-import { request } from '../request.js'
-import { ERROR_CODE, SUCCESS_CODE, ERROR_MESSAGE } from '../../../error'
-import { handleErrMsg } from '../handleErrMsg.js'
+import { request } from '../base-request.js'
+import { captureError } from '../capture-error.js'
 
-const log = debug('fideo-live-stream-getDouyuLiveUrl')
+import { SUCCESS_CODE, CRAWLER_ERROR_CODE } from '../../../code'
 
-export async function getDouyuLiveUrl(roomId, others = {}) {
+const log = debug('fideo-crawler-douyu')
+
+async function baseGetDouyuLiveUrlsPlugin(roomId, others = {}) {
   const { proxy, cookie } = others
-  log('getDouyuLiveUrl start: ', roomId, cookie, proxy)
-  try {
-    const htmlContent = (
-      await request(`https://www.douyu.com/${roomId}`, {
+
+  log('roomId:', roomId, 'cookie:', cookie, 'proxy:', proxy)
+
+  const htmlContent = (
+    await request(`https://www.douyu.com/${roomId}`, {
+      proxy,
+      headers: {
+        cookie
+      }
+    })
+  ).data
+
+  const scriptRegex = /<script\s+type=['"]text\/javascript['"]>([\s\S]*?)<\/script>/gi
+  const scriptMatches = htmlContent.match(scriptRegex)
+  for (let i = 0; i < scriptMatches.length; i++) {
+    const scriptContent = scriptMatches[i].replace(scriptRegex, '$1')
+    if (!scriptContent.includes(`ub98484234`)) continue
+
+    const m = '10000000000000000000000000001501'
+    const y = parseInt(new Date().getTime() / 1e3, 10)
+    const fn = new Function(
+      'CryptoJS',
+      `"use strict"; ${scriptContent}; return ub98484234(${roomId}, ${JSON.stringify(m)}, ${y})`
+    )
+    const S = fn(CryptoJS)
+    const v = S + '&rate=0'
+
+    const data = (
+      await request(`https://www.douyu.com/lapi/live/getH5Play/${roomId}?${v}`, {
+        method: 'POST',
         proxy,
         headers: {
           cookie
@@ -20,76 +47,34 @@ export async function getDouyuLiveUrl(roomId, others = {}) {
       })
     ).data
 
-    const scriptRegex = /<script\s+type=['"]text\/javascript['"]>([\s\S]*?)<\/script>/gi
-    const scriptMatches = htmlContent.match(scriptRegex)
-    for (let i = 0; i < scriptMatches.length; i++) {
-      const scriptContent = scriptMatches[i].replace(scriptRegex, '$1')
-      if (!scriptContent.includes(`ub98484234`)) continue
+    const multirates = data.data.multirates.sort((x, y) => x.rate - y.rate)
+    const rate = multirates[0].rate
+    const liveUrls = []
+    let _data
 
-      const m = '10000000000000000000000000001501'
-      const y = parseInt(new Date().getTime() / 1e3, 10)
-      const fn = new Function(
-        'CryptoJS',
-        `"use strict"; ${scriptContent}; return ub98484234(${roomId}, ${JSON.stringify(m)}, ${y})`
-      )
-      const S = fn(CryptoJS)
-      const v = S + '&rate=0'
-
-      const liveUrlObj = {
-        best: []
-      }
-      let minRate = Number.MAX_SAFE_INTEGER
-
-      const data = (
-        await request(`https://www.douyu.com/lapi/live/getH5Play/${roomId}?${v}`, {
-          method: 'POST',
-          proxy,
-          headers: {
-            cookie
-          }
-        })
-      ).data
-
-      const multirates = data.data.multirates.sort((x, y) => x.rate - y.rate)
-
-      for (const multirate of multirates) {
-        let _data
-        if (multirate.rate === 0) {
-          _data = data
-        } else {
-          const v = S + `&rate=${multirate.rate}`
-          const _liveRes = await fetch(`https://www.douyu.com/lapi/live/getH5Play/${roomId}?${v}`, {
-            method: 'POST'
-          })
-          _data = await _liveRes.json()
-        }
-        if (_data.msg === 'ok') {
-          const rtmp_url = _data.data.rtmp_url
-          const rtmp_live = _data.data.rtmp_live
-          const liveUrl = rtmp_url + '/' + rtmp_live
-          const liveUrls = liveUrlObj[multirate.name] || []
-          liveUrls.push(liveUrl)
-          liveUrlObj[multirate.name] = liveUrls
-          if (multirate.rate < minRate) {
-            minRate = multirate.rate
-            liveUrlObj.best = [liveUrl]
-          } else if (multirate.rate === minRate) {
-            liveUrlObj.best.push(liveUrl)
-          }
-        }
-      }
-      return {
-        code: SUCCESS_CODE,
-        liveUrlObj
-      }
+    if (rate === 0) {
+      _data = data
+    } else {
+      const v = S + `&rate=${rate}`
+      const _liveRes = await fetch(`https://www.douyu.com/lapi/live/getH5Play/${roomId}?${v}`, {
+        method: 'POST'
+      })
+      _data = await _liveRes.json()
+    }
+    if (_data.msg === 'ok') {
+      const rtmp_url = _data.data.rtmp_url
+      const rtmp_live = _data.data.rtmp_live
+      const liveUrl = rtmp_url + '/' + rtmp_live
+      liveUrls.push(liveUrl)
     }
     return {
-      code: ERROR_CODE.NOT_URLS
+      code: SUCCESS_CODE,
+      liveUrls
     }
-  } catch (e) {
-    const errMsg = e.message
-    log('getDouyuLiveUrl error: ', errMsg)
-
-    return handleErrMsg(errMsg)
+  }
+  return {
+    code: CRAWLER_ERROR_CODE.NOT_URLS
   }
 }
+
+export const getDouyuLiveUrlsPlugin = captureError(baseGetDouyuLiveUrlsPlugin)
