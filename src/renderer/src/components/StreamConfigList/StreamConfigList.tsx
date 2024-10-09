69db584e8465cb455fdadb273bfb0b9f8246f656
chen fan <1490040662@qq.com>
2024-10-09 09:03:38 +0800

    feat: tray

--- src/renderer/src/components/StreamConfigList/StreamConfigList.tsx
+++ src/renderer/src/components/StreamConfigList/StreamConfigList.tsx
@@ -197,7 +197,7 @@ export default function StreamConfigList() {
   return (
     <>
       {
-        <div className="stream-config-list flex flex-col gap-[12px] p-[24px] overflow-y-auto h-[calc(100vh-80px)]">
+        <div className="show-scrollbar flex flex-col gap-[12px] p-[24px] overflow-y-auto h-[calc(100vh-80px)]">
           {streamConfigList.map((streamConfig) => (
             <div
               key={streamConfig.title}
