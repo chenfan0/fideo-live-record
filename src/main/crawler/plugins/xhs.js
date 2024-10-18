81c25aa01029799b4f6084effc7f11367a0c2f40
chen fan <1490040662@qq.com>
2024-10-18 19:50:26 +0800

    fix: xhs

--- src/main/crawler/plugins/xhs.js
+++ src/main/crawler/plugins/xhs.js
@@ -1,11 +1,6 @@
-// example
-// https://www.xiaohongshu.com/hina/livestream/569443549099268368?timestamp=1729227035942&share_source=&share_source_id=null&source=share_out_of_app&host_id=6509435d0000000017023f36&xhsshare=WeixinSession&appuid=61129228000000000101df0a&apptime=1729227039&share_id=3c968a8ae43c4c0f8e2200626646ffa9
-
-// https://www.xiaohongshu.com/api/sns/red/live/app/v1/ecology/outside/share_info?room_id=569443549099268368
-
 import debug from 'debug'
 
-import { request, MOBILE_USER_AGENT } from '../base-request.js'
+import { request } from '../base-request.js'
 import { captureError } from '../capture-error.js'
 
 import { CRAWLER_ERROR_CODE, SUCCESS_CODE } from '../../../code'
@@ -13,9 +8,16 @@ import { CRAWLER_ERROR_CODE, SUCCESS_CODE } from '../../../code'
 const log = debug('fideo-crawler-xhs')
 
 function getRoomIdByUrl(url) {
-  return new URL(url).pathname.split('/')[3]
+  const _url = new URL(url)
+
+  if (_url.pathname.startsWith('/user/profile/')) {
+    return _url.pathname.split('/')[3]
+  }
+
+  return _url.searchParams.get('host_id')
 }
 
+// 参考 https://github.com/ihmily/DouyinLiveRecorder/blob/6508c0549c66afce7f09d30a9b5d87287f8fbf1e/douyinliverecorder/spider.py#L708
 async function baseGetXhsUrlsPlugin(roomUrl, others = {}) {
   const roomId = getRoomIdByUrl(roomUrl)
   const { proxy, cookie } = others
@@ -24,26 +26,26 @@ async function baseGetXhsUrlsPlugin(roomUrl, others = {}) {
 
   const res = (
     await request(
-      `https://www.xiaohongshu.com/api/sns/red/live/app/v1/ecology/outside/share_info?room_id=${roomId}`,
+      `https://live-room.xiaohongshu.com/api/sns/v1/live/user_status?user_id_list=${roomId}`,
       {
         headers: {
-          cookie,
-          'User-Agent': MOBILE_USER_AGENT
+          'User-Agent': 'ios/7.830 (ios 17.0; ; iPhone 15 (A2846/A3089/A3090/A3092))',
+          'xy-common-params': 'platform=iOS&sid=session.1722166379345546829388',
+          referer: 'https://app.xhs.cn/'
         }
       }
     )
   ).data
 
   console.log('res', res)
-  const status = res.data.room.status
 
-  if (status !== 0) {
+  if (res.data.length === 0) {
     return {
       code: CRAWLER_ERROR_CODE.NOT_URLS
     }
   }
 
-  const live_link = res.data.room.live_link
+  const live_link = res.data[0].live_link
 
   const flv = new URL(live_link).searchParams.get('flvUrl')
 
