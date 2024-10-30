0c08f8a1ebcc27b2eb51edf1c96eceeb0d287ea4
chen fan <1490040662@qq.com>
2024-10-30 19:24:23 +0800

    chore: update ci

--- src/main/index.ts
+++ src/main/index.ts
@@ -157,6 +157,8 @@ async function createWindow() {
 
   win = mainWindow
 
+  mainWindow.setTitle('Fideo')
+
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
   })
@@ -418,7 +420,11 @@ app.whenReady().then(async () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
-      await createWindow()
+      if (win) {
+        win.show()
+      } else {
+        await createWindow()
+      }
     }
   })
 })
